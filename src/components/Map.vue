8c33cda38366420cc0de3fcfade116e4018aa234
mahim23 <mahim23agrawal@gmail.com>
2018-08-11 18:40:48 +0530

    initial commit

---
+++ src/components/Map.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="map" class="map"></div>
+</template>
+
+<script>
+export default {
+    name: "Map",
+    props: {
+        seeds: Int32Array,
+        trees: Int32Array
+    }
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { MdButton, MdContent, MdBottomBar } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(MdBottomBar);
 
 new Vue({
   router,
